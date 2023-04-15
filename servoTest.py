ca4b1c065b31fb1058d961858bb8ab696c065f07
WesleyCowand <127052488+WesleyCowand@users.noreply.github.com>
2023-04-15 15:38:54 -0400

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -1,11 +1,13 @@
 from gpiozero import Servo
 from time import sleep
-
+import time
 servo = Servo(17)
 
 try:
 	while True:
-    		servo.value = .5
-    		sleep(1)
+    		servo.value = -.6
+    		time.sleep(8)
+		servo.value = 0
+		time.sleep(8)
 except KeyboardInterrupt:
 	print("Program stopped")
