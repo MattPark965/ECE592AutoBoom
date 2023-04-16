f021307136ba607b79d926cf66a36761d0aab301
Matt Parker <mtparke2@ncsu.edu>
2023-04-16 13:56:03 -0400

    camera resolution 1080p

--- cameraPic.py
+++ cameraPic.py
@@ -1,13 +1,16 @@
 import cv2
 
-cam=cv2.VideoCapture(0)
+cam=cv2.VideoCapture(0) #Object for video capturing
 
-while True:
-	ret, image=cam.read()
-	cv2.imshow('test',image)
-	k=cv2.waitKey(1)
-	if k!= -1:
-		break
+#Set resolution 
+cam.set(3, 1920) 
+cam.set(4, 1080)
+
+ret, image=cam.read() #Frame from camera live view
+print("Taking picture in 5 seconds!")
+time.sleep(5)
+print("Taking picture now!")
+time.sleep(1)
+cv2.imshow('test_2',image) #Show image taken
 		
-cv2.imwrite('/home/raspberrypi/test.jpg', image)
-cam.release()
+cv2.imwrite('/home/raspberrypi/test_2.jpg', image) #Save Picture
