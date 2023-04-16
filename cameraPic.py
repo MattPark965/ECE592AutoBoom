73eda9d7cfa8e8c4b92d47b51167756fbbcf9712
amydeepee <adeepee02@gmail.com>
2023-04-16 14:49:00 -0400

    cameraPic + camera_view dimensions calculated

--- cameraPic.py
+++ cameraPic.py
@@ -1,16 +1,18 @@
+#Take camera setup - 5 second sleep time before picture taken
+
 import cv2
 
 cam=cv2.VideoCapture(0) #Object for video capturing
 
-#Set resolution 
+#Set resolution based on Logitech Camera capabilities
 cam.set(3, 1920) 
 cam.set(4, 1080)
 
-ret, image=cam.read() #Frame from camera live view
+ret, image=cam.read() #Live view of camera frame
 print("Taking picture in 5 seconds!")
 time.sleep(5)
 print("Taking picture now!")
 time.sleep(1)
 cv2.imshow('test_2',image) #Show image taken
 		
-cv2.imwrite('/home/raspberrypi/test_2.jpg', image) #Save Picture
+cv2.imwrite('/home/raspberrypi/test_2.jpg', image) #Save picture to the rpi
