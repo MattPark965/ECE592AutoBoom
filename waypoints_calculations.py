6b58c1eb86af36f0fcf2bbb5de2c9dd3e8c7fc33
amydeepee <110312169+amydeepee@users.noreply.github.com>
2023-04-17 12:14:59 -0400

    slight updates in coordinates

--- waypoints_calculations.py
+++ waypoints_calculations.py
@@ -12,14 +12,16 @@ longitude = 87870 #1 degree of longitude = 87.87 km
 #Top right hand corner of box
 start = [35.726688, -78.694846]  
 
-start[0] = start[0] - (rect_height/2)/latitude
+#Set start coordinates to be in the middle of the first box
+start[0] = start[0] - (rect_height/2)/latitude 
+start[1] = start[1] + (rect_width/2)/longitude
 
 for y in range (3):
-    #print(start)
+    print(start)
     #take picture code
     for x in range(2): 
         start[0] = start[0] - (rect_height)/latitude #Move down the 3 boxes in the column
-        #print(start)
+        print(start)
         #take picture code
     start[0] = start[0] + (rect_height*2)/latitude #Move up 2 boxes 
     start[1] = start[1] + rect_width/longitude #Move to the left to start at the top of next column
