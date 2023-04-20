b73e5152929e72d99020a39677e8e06bc8636d71
Matt Parker <mtparke2@ncsu.edu>
2023-04-20 19:02:37 -0400

    changed sleep from .001 to .01

--- Working Directory/final-deliverables/TeamA22/src/rpi/waypoints.py
+++ Working Directory/final-deliverables/TeamA22/src/rpi/waypoints.py
@@ -164,7 +164,7 @@ for command in missionlist:
     copter.vehicle.simple_goto(point1)
     print("Going to waypoint")
     while(copter.distance_to_current_waypoint(command.x, command.y, command.z) > float(position_buffer)):
-        time.sleep(0.001)
+        time.sleep(0.01)
         #count = count + 1
     #dummy_take_picture(j)
     take_picture(j)
