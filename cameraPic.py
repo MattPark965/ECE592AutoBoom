019229649276972d448ebde7afa0f1a83414cce5
Matt Parker <mtparke2@ncsu.edu>
2023-04-20 17:58:52 -0400

    new changes

--- cameraPic.py
+++ cameraPic.py
@@ -48,26 +48,26 @@ j = 0
 def take_picture(j):
     print("Taking picture in 5 seconds!")
     time.sleep(5)
-    copter.condition_yaw(0) #Sets heading north
+    #copter.condition_yaw(0) #Sets heading north
     ret, image=cam.read() #Live view of camera frame
     print("Taking picture now!")
     time.sleep(1)
-    Tarps = Check_Picture_Find_Coords(image, copter.pos_alt_rel, (copter.pos_lon,copter.pos_lat))
+    #Tarps = Check_Picture_Find_Coords(image, copter.pos_alt_rel, (copter.pos_lon,copter.pos_lat))
     cv2.imshow('test_'+str(j),image) #Show image taken        
     cv2.imwrite('/home/raspberrypi/test_'+str(j)+'.jpg', image) #Save picture to the rpi
 
-    if Tarps is not None: #This loop executed if tarp is found
-        cv2.circle(image, (Tarps[0], Tarps[1]), radius = 5, color = (0, 255, 0), thickness = -1)
-        cv2.imwrite('/home/raspberrypi/test_'+str(j)+'marked'+'.jpg', image) #Save picture to the rpi
+    # if Tarps is not None: #This loop executed if tarp is found
+    #     cv2.circle(image, (Tarps[0], Tarps[1]), radius = 5, color = (0, 255, 0), thickness = -1)
+    #     cv2.imwrite('/home/raspberrypi/test_'+str(j)+'marked'+'.jpg', image) #Save picture to the rpi
 
-        packet = {
-                    "target_x" : Tarps[0],
-                    "target_y" : Tarps[1]
-                  }   
-        # convert to bytes for socket data transfer
-        packet_bytes = json.dumps(packet).encode('utf-8')
-        # send data to the GCS
-        # s.sendto(packet_bytes, (gcs_ip, gcs_port))
+    #     packet = {
+    #                 "target_x" : Tarps[0],
+    #                 "target_y" : Tarps[1]
+    #               }   
+    #     # convert to bytes for socket data transfer
+    #     packet_bytes = json.dumps(packet).encode('utf-8')
+    #     # send data to the GCS
+    #     # s.sendto(packet_bytes, (gcs_ip, gcs_port))
 take_picture(j)
 j=j+1   
 
