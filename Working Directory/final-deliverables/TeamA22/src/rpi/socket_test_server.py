30b2f4255d524b9c9783a0d51988254ffbc4fb0b
Matt Parker <mtparke2@ncsu.edu>
2023-04-22 19:12:29 -0400

    updated

--- Working Directory/final-deliverables/TeamA22/src/rpi/socket_test_server.py
+++ Working Directory/final-deliverables/TeamA22/src/rpi/socket_test_server.py
@@ -1,21 +1,24 @@
 import socket
 
 # Set up IP address and port
-SERVER_IP = '127.0.0.1'  # replace with the IP address of the server
+LOCAL_IP = '127.0.0.1'  # replace with the IP address of the server
 PORT = 5500  # replace with the port number used by the server
 
 # Create socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Set socket timeout to 1 second
-s.settimeout(1)
+# s.settimeout(1)
+s.bind(LOCAL_IP, PORT)
 
 # Attempt to connect to server
-try:
-    s.connect((SERVER_IP, PORT))
-    print('Server is running and listening on', SERVER_IP, 'port', PORT)
-except socket.error:
-    print('Server is not running or not listening on', SERVER_IP, 'port', PORT)
+while True:
+    try:
+        message, address = s.recvfrom(1024)
+        print(message)
+        print('Server is running and listening on', address, 'port', PORT)
+    except socket.error:
+        print('Server is not running or not listening on', address, 'port', PORT)
 
 # Close socket connection
-s.close()
+# s.close()
