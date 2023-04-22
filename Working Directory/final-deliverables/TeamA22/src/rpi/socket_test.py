6f77e3aa28da91e450840f8d971eed160992ca5d
Matt Parker <mtparke2@ncsu.edu>
2023-04-22 19:01:22 -0400

    testing socket server

--- Working Directory/final-deliverables/TeamA22/src/rpi/socket_test.py
+++ Working Directory/final-deliverables/TeamA22/src/rpi/socket_test.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 # Set up IP addresses and port
 SERVER_IP = '127.0.0.1'  # replace with the IP address of the server
@@ -6,21 +7,23 @@ CLIENT_IP = '10.154.60.204'  # replace with the IP address of the client
 PORT = 5500  # replace with any available port number
 
 # Create socket object
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # DGRAM MAKES IT UDP
 
 # Bind socket to client IP and port
-s.bind((CLIENT_IP, PORT))
+s.bind((SERVER_IP, PORT))
 
 # Connect to server IP and port
-s.connect((SERVER_IP, PORT))
+#s.connect((SERVER_IP, PORT))
 
 # Send message to server
-message = 'Hello, server!'
-s.sendall(message.encode())
+while True:
+    message = 'Hello, server!'
+    s.sendall(message.encode())
+    time.sleep(2) 
 
 # Receive message from server
 data = s.recv(1024)
 print('Received from server:', data.decode())
 
 # Close socket connection
-s.close()
+#s.close()
