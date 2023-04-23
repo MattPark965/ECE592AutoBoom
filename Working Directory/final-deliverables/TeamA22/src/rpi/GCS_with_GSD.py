24693238c717422864df8ffc4847fd8bb76f89e2
amydeepee <110312169+amydeepee@users.noreply.github.com>
2023-04-23 14:03:35 -0400

    integration

--- Working Directory/final-deliverables/TeamA22/src/rpi/GCS_with_GSD.py
+++ Working Directory/final-deliverables/TeamA22/src/rpi/GCS_with_GSD.py
@@ -187,10 +187,11 @@ def receive_message():
     # Instead of timing out, we can keep retrying for a message.
     while True:
         time.sleep(2)    
+        msg, address = s.recvfrom(4096)
+        print(msg)
+        print('Server is running and listening on', address, 'port', PORT)
         try:
             msg, address = s.recvfrom(4096)
-            print(msg)
-            print('Server is running and listening on', address, 'port', PORT)
             # recvfrom is a UDP command, recv is a TCP command, takes in argument (Buffer Size).
             # msg = s.recvfrom(4096)
             data = msg[0]
