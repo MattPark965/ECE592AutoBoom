2ac238e15e9ac101223d88673c9d743aaf511c57
Victor <vminin@ncsu.edu>
2023-04-25 17:42:01 -0400

    LilVickyBabyFine fixed center offset

--- Working Directory/final-deliverables/TeamA22/src/rpi/tarpdetector.py
+++ Working Directory/final-deliverables/TeamA22/src/rpi/tarpdetector.py
@@ -37,7 +37,7 @@ def detect_blue_cluster(img, lower_blue, upper_blue):
                 cY = int(M["m01"] / M["m00"])
                 blue_cluster_centertemp = (cX, cY)
     img_center = (img.shape[1] // 2, img.shape[0] // 2)
-    if blue_cluster_center is not None:
+    if blue_cluster_centertemp is not None:
         offset1 = (img_center[0] - blue_cluster_centertemp[0])
         offset2 = (img_center[1] - blue_cluster_center[1])
         blue_cluster_center = ((blue_cluster_centertemp[0] + offset1), (blue_cluster_centertemp[1] + offset2))
